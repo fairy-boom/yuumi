2a702abbaaa6497784129e18794e060e63601eae
tookbra <tookbra@outlook.com>
2020-06-22 15:46:50 +0800

    :sparkles: add mybatis base mapper

---
+++ yuumi-mybatis-starter/src/main/java/org/okboom/yuumi/mybatis/mapper/BaseMapper.java
@@ -0,0 +1,9 @@
+package org.okboom.yuumi.mybatis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.Mapper;
+
+/**
+ * @author tookbra
+ */
+public interface BaseMapper<T> extends Mapper<T> {
+}
